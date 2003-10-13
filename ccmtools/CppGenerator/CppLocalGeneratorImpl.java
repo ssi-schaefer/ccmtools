b3a0deb7d9d4cf2ec8ebb3a04e27c40b207aba9c
teiniker <>
2003-10-13 07:40:14 +0000

    fixed a small bug in the CppLocalGenerator

--- ccmtools/CppGenerator/CppLocalGeneratorImpl.java
+++ ccmtools/CppGenerator/CppLocalGeneratorImpl.java
@@ -261,7 +261,8 @@ public class CppLocalGeneratorImpl
             f = new ArrayList();
             f.add(base + "_share"); f.add(node_name + "_share.h"); files.add(f);
 
-            if (! current_node instanceof MHomeDef) {
+	    //            if (!(current_node instanceof MHomeDef)) {
+	    if (current_node instanceof MHomeDef) {
                 f = new ArrayList();
                 f.add("src"); f.add(node_name + "_entry.h"); files.add(f);
             }
@@ -275,6 +276,8 @@ public class CppLocalGeneratorImpl
                 f = new ArrayList(); f.add("src"); f.add(""); files.add(f);
                 f = new ArrayList(); f.add("src"); f.add(""); files.add(f);
             }
+	    
+	    System.out.println(f); //!!!!!!!!!
 
         } else if ((current_node instanceof MInterfaceDef)
                    || (current_node instanceof MStructDef)
