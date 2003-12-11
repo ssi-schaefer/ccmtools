550d94ece20ce642832a89c64be25c52d3f5ff44
teiniker <>
2003-12-11 15:32:06 +0000

    - The ccmtools do not need to link the micoccm2.3.11.so lib (a segmentation fault   can occur because of common global identifiers in CCM.cc) + A README file explains the installation of the component environment.

--- ccmtools/environment/external/mico/Makefile.py
+++ ccmtools/environment/external/mico/Makefile.py
@@ -5,9 +5,9 @@ EXTERNAL_LIBRARY(
     defroot='/usr/local',
     incdir='include',
     libdir='lib',
-    lib_bases=['micoccm2.3.11',
+    lib_bases=['mico2.3.11',
                'micocoss2.3.11',
-               'mico2.3.11',
+	       #'micoccm2.3.11',
                # mico needs dlopen() and friends
                'dl'],
     provide=['CORBA.h'],
