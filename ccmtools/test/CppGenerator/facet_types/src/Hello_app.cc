b54917d8b8202194f5dd2881e2452d482dfbf31e
teiniker <>
2004-02-06 12:23:24 +0000

    + Changed the source code of the facet_types test case to fix some   errors because of changing localComponents to LocalComponents.

--- ccmtools/test/CppGenerator/facet_types/src/Hello_app.cc
+++ ccmtools/test/CppGenerator/facet_types/src/Hello_app.cc
@@ -249,8 +249,8 @@ CCM_Hello_impl::~CCM_Hello_impl (  )
 
 
 void
-CCM_Hello_impl::set_session_context ( localComponents::SessionContext* context )
-  throw ( localComponents::CCMException )
+CCM_Hello_impl::set_session_context ( LocalComponents::SessionContext* context )
+  throw ( LocalComponents::CCMException )
 {
   DEBUGNL ( " CCM_Hello_impl->set_session_context (  )" );
   ctx = dynamic_cast<CCM_Hello_Context*>(context);
@@ -258,21 +258,21 @@ CCM_Hello_impl::set_session_context ( localComponents::SessionContext* context )
 
 void
 CCM_Hello_impl::ccm_activate (  )
-  throw ( localComponents::CCMException )
+  throw ( LocalComponents::CCMException )
 {
   DEBUGNL ( " CCM_Hello_impl->ccm_activate (  )" );
 }
 
 void
 CCM_Hello_impl::ccm_passivate (  )
-  throw ( localComponents::CCMException )
+  throw ( LocalComponents::CCMException )
 {
   DEBUGNL ( " CCM_Hello_impl->ccm_passivate (  )" );
 }
 
 void
 CCM_Hello_impl::ccm_remove (  )
-  throw ( localComponents::CCMException )
+  throw ( LocalComponents::CCMException )
 {
   DEBUGNL ( " CCM_Hello_impl->ccm_remove (  )" );
 }
