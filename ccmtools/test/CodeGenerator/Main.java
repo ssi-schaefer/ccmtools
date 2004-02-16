d932d6bdd5912badbf3ccaad73ff195a8107f049
teiniker <>
2004-02-16 12:29:39 +0000

    + Added package definition in the Main.java file

--- ccmtools/test/CodeGenerator/Main.java
+++ ccmtools/test/CodeGenerator/Main.java
@@ -19,6 +19,8 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+package ccmtools.test.CodeGenerator;
+
 import ccmtools.CodeGenerator.GraphTraverser;
 import ccmtools.CodeGenerator.CCMMOFGraphTraverserImpl;
 import ccmtools.CodeGenerator.NodeHandler;
