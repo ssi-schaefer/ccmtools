35d684388768893b8d3da025b7cc5c3fba80056a
teiniker <>
2004-02-19 20:48:47 +0000

    - moved uml2idl to java-environment/src/

--- ccmtools/uml2idl/make_tool.sh
+++
@@ -1,4 +0,0 @@
-cd ../..
-javac -classpath /tmp/uml_parser.jar:$CLASSPATH ccmtools/uml2idl/*.java
-jar -cf /tmp/uml2idl.jar ccmtools/uml2idl/*.class
-rm ccmtools/uml2idl/*.class
--- ccmtools/uml2idl/run.sh
+++
@@ -1,1 +0,0 @@
-java -cp /tmp/uml_parser.jar:/tmp/uml2idl.jar:$CLASSPATH ccmtools.uml2idl.Main $*
