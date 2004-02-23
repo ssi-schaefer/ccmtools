6dda1c5458194e903de7aad47006a8ecf3744905
rlechner <>
2004-02-23 13:07:11 +0000

    .

--- java-environment/make_it.sh
+++ java-environment/make_it.sh
@@ -9,13 +9,14 @@
 ROOT=$PWD
 LIB=$ROOT/lib
 BUILD=$ROOT/tmp
+SOURCE=$ROOT/src
 mkdir $BUILD
 
 
 #
 #   dtd2java
 #
-cd $ROOT/dtd2java
+cd $SOURCE/dtd2java
 java -classpath $LIB/antlr.jar antlr.Tool dtd.g
 javac -classpath $LIB/antlr.jar -d $BUILD *.java
 rm DtdLexer.java DtdParser.java DTDTokenTypes.java DTDTokenTypes.txt
@@ -27,12 +28,12 @@ jar cf $LIB/dtd2java.jar dtd2java/*.class
 #
 #   uml_parser, uml2idl
 #
-cd $ROOT
+cd $SOURCE
 rm -rf uml_parser
 java -classpath $LIB/antlr.jar:$LIB/dtd2java.jar dtd2java.Main dtd2java/UML.dtd uml_parser
 rm dtd2java/UML.dtd.PP.dtd
 javac -d $BUILD uml_parser/*.java uml_parser/uml/*.java
-cd $ROOT/uml2idl
+cd $SOURCE/uml2idl
 javac -classpath $BUILD -d $BUILD *.java
 cd $BUILD
 rm -f $LIB/uml2idl.jar
