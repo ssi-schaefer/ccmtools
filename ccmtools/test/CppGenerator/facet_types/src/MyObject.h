3d52ceb41543edb38adae25c92b7cd59e368ab89
teiniker <>
2004-04-05 19:33:48 +0000

    + moved Hello.idl to Test.idl + implemented a test case for interfaces as parameters

---
+++ ccmtools/test/CppGenerator/facet_types/src/MyObject.h
@@ -0,0 +1,27 @@
+
+#ifndef __MY_OBJECT__H__
+#define __MY_OBJECT__H__
+
+#include <iostream>
+#include <CCM_Local/Console.h>
+
+class MyObject 
+  : virtual public CCM_Local::CCM_Console
+{
+public:
+  MyObject();
+
+  std::string prompt() 
+    throw(LocalComponents::CCMException);
+
+  void prompt(const std::string value) 
+    throw(LocalComponents::CCMException);
+
+  long println(const std::string& msg);
+  
+private: 
+  std::string prompt_;
+};
+
+
+#endif
