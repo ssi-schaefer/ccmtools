59b77fdbe3e8cc44fc5ac2d6d25f445faada9bae
teiniker <>
2004-05-17 16:26:26 +0000

    + changed exception test (Hello.idl to Test.idl)

--- ccmtools/test/CppGenerator/facet_exception.sh
+++ ccmtools/test/CppGenerator/facet_exception.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 ${top_srcdir}/test/CppGenerator/test-loader.sh \
-  "facet_exception" " " "${srcdir}/../facet_exception/Hello.idl"
+  "facet_exception" " " "${srcdir}/../facet_exception/Test.idl"
 
