b13fd7da9674a4f0ffcafd190890eb3b8a952baa
teiniker <>
2004-08-31 15:20:42 +0000

    Changed debug.cc to support DEBUL_LEVEL_IDENTIFIER_WITH_UNDERLINES

--- cpp-environment/WX_Utils/code/debug.cc
+++ cpp-environment/WX_Utils/code/debug.cc
@@ -28,6 +28,7 @@ is_levelchar(
    char c)
 {
    switch (c) {
+      case '_': case ':': 
       case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
       case 'G': case 'H': case 'I': case 'J': case 'K': case 'L':
       case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R':
