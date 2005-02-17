c55629af9c19e44b328e336c1ca7e36bf083def4
teiniker <>
2005-02-17 15:19:49 +0000

    changed TimerEveluation output

--- cpp-environment/wx/measurement/TimerEvaluation.cc
+++ cpp-environment/wx/measurement/TimerEvaluation.cc
@@ -24,11 +24,22 @@ TimerEvaluation::getTimerResult(const Timer& timer,
 				const long loops, 
 				const long size)
 {
+  /*
   ostringstream os;  
   os << "loops(" << loops << ") size(" << size << ") " 
      << timer << endl;  
   return os.str();
+  */
+
+  ostringstream os;
+  os << "loops(" << loops << ") "
+     << "size("  << size << ") "
+     << "real("  << timer.getRealTime() << ")s "
+     << "call("  << (timer.getRealTime()/loops)*1.0e6 << ")us"
+     << endl;  
+  return os.str();
 }
+
 
 
 } // /namespace
