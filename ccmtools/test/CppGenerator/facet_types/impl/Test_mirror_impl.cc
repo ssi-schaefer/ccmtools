8fd5b7e55ebadb5085305ea2934a1b375f77089e
teiniker <>
2005-06-03 09:39:06 +0000

    added Makefiles to some local test cases

--- ccmtools/test/CppGenerator/facet_types/impl/Test_mirror_impl.cc
+++ ccmtools/test/CppGenerator/facet_types/impl/Test_mirror_impl.cc
@@ -15,7 +15,7 @@
 #include <cassert>
 #include <iostream>
 #include <WX/Utils/debug.h>
-#include <CCM_Local/toString.h>
+#include <CCM_Local/Debug.h>
 
 #include "Test_mirror_impl.h"
 #include "MyObject.h"
@@ -63,70 +63,70 @@ CCM_Test_mirror_impl::ccm_activate (  )
     // basic types test cases
     short short_2=3, short_3, short_r;
     short_r = type_test->op_b1(7,short_2, short_3);
-    cout << CCM_Local::toString(short_r) << endl;
+    cout << CCM_Local::ccmDebug(short_r,2) << endl; //!!!
     assert(short_2 == 7);
     assert(short_3 == 3);
     assert(short_r == 3+7);
     
     long long_2=3, long_3, long_r;
     long_r = type_test->op_b2(7,long_2, long_3);
-    cout << CCM_Local::toString(long_r) << endl;
+    cout << CCM_Local::ccmDebug(long_r,2) << endl; //!!!
     assert(long_2 == 7);
     assert(long_3 == 3);
     assert(long_r == 3+7);
     
     unsigned short ushort_2=3, ushort_3, ushort_r;
     ushort_r = type_test->op_b3(7,ushort_2, ushort_3);
-    cout << CCM_Local::toString(ushort_r) << endl;
+    cout << CCM_Local::ccmDebug(ushort_r,2) << endl; //!!!
     assert(ushort_2 == 7);
     assert(ushort_3 == 3);
     assert(ushort_r == 3+7);
     
     unsigned long ulong_2=3, ulong_3, ulong_r;
     ulong_r = type_test->op_b4(7,ulong_2, ulong_3);
-    cout << CCM_Local::toString(ulong_r) << endl;
+    cout << CCM_Local::ccmDebug(ulong_r,2) << endl; //!!!
     assert(ulong_2 == 7);
     assert(ulong_3 == 3);
     assert(ulong_r == 3+7);
     
     float float_2=3.0, float_3, float_r;
     float_r = type_test->op_b5(7.0,float_2, float_3);
-    cout << CCM_Local::toString(float_r) << endl;
+    cout << CCM_Local::ccmDebug(float_r,2) << endl; //!!!
     assert(float_2 == 7.0);
     assert(float_3 == 3.0);
     assert(float_r == 3.0+7.0);
     
     double double_2=3.0, double_3, double_r;
     double_r = type_test->op_b6(7.0,double_2, double_3);
-    cout << CCM_Local::toString(double_r) << endl;
+    cout << CCM_Local::ccmDebug(double_r,2) << endl; //!!!
     assert(double_2 == 7.0);
     assert(double_3 == 3.0);
     assert(double_r == 3.0+7.0);
     
     char char_2=3, char_3, char_r;
     char_r = type_test->op_b7(7,char_2, char_3);
-    cout << CCM_Local::toString(char_r) << endl;
+    cout << CCM_Local::ccmDebug(char_r,2) << endl; //!!!
     assert(char_2 == 7);
     assert(char_3 == 3);
     assert(char_r == 3+7);
 
     string string_2="drei", string_3, string_r;
     string_r = type_test->op_b8("sieben",string_2, string_3);
-    cout << CCM_Local::toString(string_r) << endl;
+    cout << CCM_Local::ccmDebug(string_r,2) << endl; //!!!
     assert(string_2 == "sieben");
     assert(string_3 == "drei");
     assert(string_r == "dreisieben");
     
     bool bool_2=false, bool_3, bool_r;
     bool_r = type_test->op_b9(true, bool_2, bool_3);
-    cout << CCM_Local::toString(bool_r) << endl;
+    cout << CCM_Local::ccmDebug(bool_r,2) << endl; //!!!
     assert(bool_2 == true);
     assert(bool_3 == false);
     assert(bool_r == false && true);
     
     unsigned char uchar_2=3, uchar_3, uchar_r;
     uchar_r = type_test->op_b10(7,uchar_2, uchar_3);
-    cout << CCM_Local::toString(uchar_r) << endl;
+    cout << CCM_Local::ccmDebug(uchar_r,2) << endl; //!!!
     assert(uchar_2 == 7);
     assert(uchar_3 == 3);
     assert(uchar_r == 3+7);
@@ -135,14 +135,14 @@ CCM_Test_mirror_impl::ccm_activate (  )
   {
     wchar_t char_2=3, char_3, char_r;
     char_r = type_test->op_b11(7,char_2, char_3);
-    cout << CCM_Local::toString(char_r) << endl;
+    cout << CCM_Local::ccmDebug(char_r,2) << endl; //!!!
     assert(char_2 == 7);
     assert(char_3 == 3);
     assert(char_r == 3+7);
 
     wstring string_2= L"drei", string_3, string_r;
     string_r = type_test->op_b12(L"sieben",string_2, string_3);
-    cout << CCM_Local::toString(string_r) << endl;
+    cout << CCM_Local::ccmDebug(string_r,2) << endl; //!!!
     assert(string_2 == L"sieben");
     assert(string_3 == L"drei");
     assert(string_r == L"dreisieben");
@@ -153,6 +153,7 @@ CCM_Test_mirror_impl::ccm_activate (  )
     // test case: typedef long time_t;
     CCM_Local::time_t time_t_2 = 3, time_t_3, time_t_r;
     time_t_r = type_test->op_u1(7,time_t_2, time_t_3);
+    cout << CCM_Local::ccmDebug(time_t_r,2) << endl; //!!!
     assert(time_t_2 == 7);
     assert(time_t_3 == 3);
     assert(time_t_r == 3+7);
@@ -161,6 +162,7 @@ CCM_Test_mirror_impl::ccm_activate (  )
     Color Color_2,Color_3, Color_r;
     Color_2 = Color(blue);
     Color_r = type_test->op_u2(Color(red),Color_2, Color_3);
+    cout << CCM_Local::ccmDebug(Color_3,2) << endl; //!!!
     assert(Color_2 == Color(red));
     assert(Color_3 == Color(blue));
     assert(Color_r == Color(red));
@@ -170,7 +172,7 @@ CCM_Test_mirror_impl::ccm_activate (  )
     Pair_1.key = "a"; Pair_1.value = 1.0;
     Pair_2.key = "b"; Pair_2.value = 2.0;
     Pair_r = type_test->op_u3(Pair_1,Pair_2,Pair_3);
-    cout << CCM_Local::toString(Pair_r) << endl;
+    cout << CCM_Local::ccmDebug(Pair_r,2) << endl; //!!!
     assert(Pair_3.key == "b");
     assert(Pair_2.key == "a");
     assert(Pair_r.key == "ab");
@@ -187,6 +189,7 @@ CCM_Test_mirror_impl::ccm_activate (  )
       map_2.push_back(p2);
     }
     map_r = type_test->op_u4(map_1,map_2,map_3);
+    cout << CCM_Local::ccmDebug(map_r,2) << endl; //!!!
     for(unsigned int i=0;i<map_r.size();i++) {
       Pair p = map_r.at(i);
       assert(p.value == (long)i);
@@ -207,6 +210,7 @@ CCM_Test_mirror_impl::ccm_activate (  )
       Array_2.at(i) = i+i;
     }
     Array_r = type_test->op_u5(Array_1,Array_2,Array_3);
+    cout << CCM_Local::ccmDebug(Array_r,2) << endl; //!!!
     for(int i=0;i<10;i++) {
       assert(Array_r.at(i) == i);
       assert(Array_2.at(i) == i);
