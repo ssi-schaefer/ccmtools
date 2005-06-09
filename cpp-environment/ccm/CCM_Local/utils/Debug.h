87bc8978f0e14706581b7e0d6c1aeb3d01c775bc
teiniker <>
2005-06-09 11:43:59 +0000

    added ccmDebug() functions for C++ primitive types

---
+++ cpp-environment/ccm/CCM_Local/utils/Debug.h
@@ -0,0 +1,184 @@
+/* -*- mode: C++; c-basic-offset: 4 -*-
+ *
+ * CCM Tools : CCM_Local/utils
+ * Egon Teiniker <egon.teiniker@tugraz.at>
+ * copyright (c) 2002 - 2005 Salomon Automation
+ *
+ * $Id$
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef __CCM__LOCAL__TOSTRING__H__
+#define __CCM__LOCAL__TOSTRING__H__
+
+#ifdef WXDEBUG
+
+#include <iostream>
+
+namespace CCM_Local {
+
+    /**
+     * Create a string with n*TAB spaces that can be used as
+     * indent for debug messages.
+     */
+    inline 
+    std::string 
+    doIndent(int n)
+    {
+	const std::string TAB = "    "; 
+	std::string space = "";
+
+	for(int i=0;i<n; i++) {
+	    space += TAB;
+	}
+	return space;
+    }
+
+    /**
+     * Convert C++ basic types into string representations which can
+     * be used for debug issues.
+     **/
+    
+    inline 
+    std::string 
+    ccmDebug(const bool& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	if(in == true) {
+	    os << "true:boolean";
+	}
+	else {
+	    os << "false:boolean";
+	}
+	return os.str();
+    } 
+    
+    inline 
+    std::string 
+    ccmDebug(const char& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << (int)in << ":char";
+	return os.str();
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const double& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << in << ":double";
+	return os.str();
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const float& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << in << ":float";
+	return os.str();
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const long& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << in << ":long";
+	return os.str();
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const unsigned char& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << (int)in << ":unsigned char";
+	return os.str();   
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const short& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << in << ":short";
+	return os.str();
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const std::string& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << "\"" << in << "\"" << ":string";
+	return os.str();  
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const unsigned long& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << in << ":unsigned long";
+	return os.str();         
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const unsigned short& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << in << ":unsigned short";
+	return os.str();         
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const wchar_t& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << (int)in << ":wchar_t";
+	return os.str();
+    }
+    
+    inline 
+    std::string 
+    ccmDebug(const std::wstring& in, int indent = 0)
+    {
+	std::ostringstream os;
+	os << doIndent(indent);
+	os << ":wstring"; // TODO !!!!!!
+	return os.str();
+    }
+    
+} // /namespace CCM_Local
+
+#endif // WXDEBUG
+
+#endif // __CCM__LOCAL__TOSTRING__H__
