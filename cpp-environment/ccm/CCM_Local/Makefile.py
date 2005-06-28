8d0fdee802c1abeab5e652217b3df3bfdce9572a
jfasch <>
2005-06-28 16:24:48 +0000

    make this an optional package root

---
+++ cpp-environment/ccm/CCM_Local/Makefile.py
@@ -0,0 +1,12 @@
+# according to the INSTALL file, this directory is built as a
+# subdirectory of the cpp-environment package. however, sometimes one
+# wants to make this directory and its descendants one separate
+# package.
+
+try:
+    PACKAGE_NAME('ccm-local')
+    PACKAGE_VERSION('0.5.1')
+except Error, e:
+    # we'll get here if this is not the package root, and have to
+    # ignore this.
+    pass
