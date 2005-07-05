b6d20e74baa6474e8aec84f1d44ef4bb242dbfd6
teiniker <>
2005-07-05 09:05:28 +0000

    bugfix: ccmDebug should only be used with DEBUGNL macros

--- ccmtools/test/CppGenerator/facet_exception/impl/Test_mirror_impl.cc
+++ ccmtools/test/CppGenerator/facet_exception/impl/Test_mirror_impl.cc
@@ -64,14 +64,12 @@ CCM_Test_mirror_impl::ccm_activate()
         assert(0);
     }
     catch(CCM_Local::Error& e) {
-      /*
         cout << "OK: error exception catched! ";
         cout << "(" 
 	     << e.info[0].code << ", " 
 	     << e.info[0].message << ")" 
 	     << endl;
-      */
-      cout << ccmDebug(e) << endl;
+      LDEBUGNL(CCM_LOCAL, ccmDebug(e));
     }
     
     try {
@@ -80,8 +78,8 @@ CCM_Test_mirror_impl::ccm_activate()
         assert(0);
     }
     catch(CCM_Local::SuperError& e) {
-      // cout << "OK: super_error exception catched!" << endl;
-      cout << ccmDebug(e) << endl;
+      cout << "OK: super_error exception catched!" << endl;
+      LDEBUGNL(CCM_LOCAL, ccmDebug(e));
     }
   
     try {
@@ -90,8 +88,8 @@ CCM_Test_mirror_impl::ccm_activate()
         assert(0);
     }
     catch(CCM_Local::FatalError& e) {
-      // cout << "OK: fatal_error exception catched!" << endl;
-      cout << ccmDebug(e) << endl;
+      cout << "OK: fatal_error exception catched!" << endl;
+      LDEBUGNL(CCM_LOCAL, ccmDebug(e));
     }
 }
 
