9627552d7c43b6f441abb434fc55038d74110989
teiniker <>
2005-08-04 07:43:37 +0000

    fixed version number in Makefile.py

--- cpp-environment/ccm/CCM_Local/Makefile.py
+++ cpp-environment/ccm/CCM_Local/Makefile.py
@@ -5,7 +5,7 @@
 
 try:
     PACKAGE_NAME('ccm-local')
-    PACKAGE_VERSION('0.5.1')
+    PACKAGE_VERSION('0.5.2')
 except Error, e:
     # we'll get here if this is not the package root, and have to
     # ignore this.
