cafd52a20cdc57c7cc3717fd38c2892476fb0e5b
teiniker <>
2005-08-18 07:58:21 +0000

    added exception declarations

--- ccmtools/test/CppGenerator/receptacle_object/test/ReceptacleObject.cc
+++ ccmtools/test/CppGenerator/receptacle_object/test/ReceptacleObject.cc
@@ -16,6 +16,7 @@ ReceptacleObject::~ReceptacleObject()
 
 long 
 ReceptacleObject::op_b1(const long p1, long& p2, long& p3)
+  throw (ccm::local::Components::CCMException)
 {
   DEBUGNL(" ReceptacleObject::op_b1()");
   cout << " ReceptacleObject::op_b1()" << endl;
@@ -26,6 +27,7 @@ ReceptacleObject::op_b1(const long p1, long& p2, long& p3)
 
 string 
 ReceptacleObject::op_b2(const std::string& p1, std::string& p2, std::string& p3)
+  throw (ccm::local::Components::CCMException)
 {
   DEBUGNL(" ReceptacleObject::op_b2()");
   cout << " ReceptacleObject::op_b2()" << endl;
--- ccmtools/test/CppGenerator/receptacle_object/test/ReceptacleObject.h
+++ ccmtools/test/CppGenerator/receptacle_object/test/ReceptacleObject.h
@@ -8,6 +8,10 @@ class ReceptacleObject
   ReceptacleObject();
   virtual ~ReceptacleObject();
 
-  virtual long op_b1(const long p1, long& p2, long& p3);
-  virtual std::string op_b2(const std::string& p1, std::string& p2, std::string& p3);
+  virtual long op_b1(const long p1, long& p2, long& p3)
+     throw (ccm::local::Components::CCMException);
+  virtual std::string op_b2(const std::string& p1, 
+			    std::string& p2, 
+			    std::string& p3)
+    throw (ccm::local::Components::CCMException);
 };
