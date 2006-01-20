25d98903750bba813c39b6a48afb4bddbde81c2e
teiniker <>
2006-01-20 14:02:46 +0000

    added test case

---
+++ ccmtools/test/JavaClientLib/facet_module_simple/src/Client.java
@@ -0,0 +1,59 @@
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.StringHolder;
+
+import world.ccm.local.*;
+import ccm.local.ServiceLocator;
+
+
+public class Client
+{
+    public static void main(String[] args)
+    {
+        ORB orb = ORB.init(args, null);
+        try {
+	    // Set up the ServiceLocator singleton
+            ServiceLocator.instance().setCorbaOrb(orb);
+	    
+            // Register homes to the HomeFinder
+            ccm.local.HomeFinder.instance()
+		.register_home(TestHomeFactory.create(),"myTestHome");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+	
+	try
+	{
+	    System.out.println("Client");
+	    
+	    ccm.local.Components.HomeFinder homeFinder = 
+		ccm.local.HomeFinder.instance();
+	    TestHome home = 
+		(TestHome) homeFinder.find_home_by_name("myTestHome");
+
+	    Test component = home.create();	    
+	    component.configuration_complete();
+
+	    I2 iface = component.provide_my_facet();
+
+	    {
+		String s = "1234567890";
+		int size = iface.op1(s);
+		assert(s.length() == size);
+	    }
+
+	    component.remove();
+	    System.out.println("OK!");
+	}
+	catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+	
+
+       // Unregister homes from the HomeFinder
+        ccm.local.HomeFinder.instance().unregister_home("myTestHome");
+	System.exit(0);
+    }
+}
