360d3c15ecdef0bef1e8749a06dd6f2b10ee21c2
teiniker <>
2006-01-24 16:48:22 +0000

    *** empty log message ***

--- ccmtools/test/JavaClientLib/receptacle_module_types/src/client/myBasicTypeImpl.java
+++ ccmtools/test/JavaClientLib/receptacle_module_types/src/client/myBasicTypeImpl.java
@@ -1,7 +1,14 @@
 package client;
 
-
+import org.omg.CORBA.IntHolder;
+import org.omg.CORBA.ShortHolder;
 import ccm.local.Components.CCMException;
+import org.omg.CORBA.FloatHolder;
+import org.omg.CORBA.DoubleHolder;
+import org.omg.CORBA.CharHolder;
+import org.omg.CORBA.StringHolder;
+import org.omg.CORBA.BooleanHolder;
+import org.omg.CORBA.ByteHolder;
 
 public class myBasicTypeImpl
     implements world.europe.austria.ccm.local.BasicTypeInterface
@@ -11,79 +18,79 @@ public class myBasicTypeImpl
     {
     }
 
-    public short f1 (short p1, org.omg.CORBA.ShortHolder p2, org.omg.CORBA.ShortHolder p3)
+    public short f1 (short p1, ShortHolder p2, ShortHolder p3)
     {
-	System.out.println("myBasicTypeImpl.f1()");
-	return 0;
+    	System.out.println("myBasicTypeImpl.f1()");
+    	return 0;
     }
     
 
-    public int f2 (int p1, org.omg.CORBA.IntHolder p2, org.omg.CORBA.IntHolder p3);
-	throws CCMException
-    {
-	System.out.println("myBasicTypeImpl.f2()");
-	p3.value = p2.value;
-	p2.value = p1;
-	int result = p3.value + p1;
-	return result;
-    }
+    public int f2(int p1, IntHolder p2, IntHolder p3) 
+    	throws CCMException
+	{
+		System.out.println("myBasicTypeImpl.f2()");
+		p3.value = p2.value;
+		p2.value = p1;
+		int result = p3.value + p1;
+		return result;
+	}
 
 
-    public short f3 (short p1, org.omg.CORBA.ShortHolder p2, org.omg.CORBA.ShortHolder p3)
-    {
-	System.out.println("myBasicTypeImpl.f3()");
-	return 0;
-    }
+    public short f3(short p1, ShortHolder p2, ShortHolder p3)
+	{
+		System.out.println("myBasicTypeImpl.f3()");
+		return 0;
+	}
 
 
-    public int f4 (int p1, org.omg.CORBA.IntHolder p2, org.omg.CORBA.IntHolder p3)
-    {
-	System.out.println("myBasicTypeImpl.f4()");
-	return 0;
-    }
+    public int f4(int p1, IntHolder p2, IntHolder p3)
+	{
+		System.out.println("myBasicTypeImpl.f4()");
+		return 0;
+	}
 
 
-    public float f5 (float p1, org.omg.CORBA.FloatHolder p2, org.omg.CORBA.FloatHolder p3)
-    {
-	System.out.println("myBasicTypeImpl.f5()");
-	return 0.0;
-    }
+    public float f5(float p1, FloatHolder p2, FloatHolder p3)
+	{
+		System.out.println("myBasicTypeImpl.f5()");
+		return (float) 0.0;
+	}
 
 
-    public double f6 (double p1, org.omg.CORBA.DoubleHolder p2, org.omg.CORBA.DoubleHolder p3)
-    {
-	System.out.println("myBasicTypeImpl.f6()");
-	return 0.0;
-    }
+    public double f6(double p1, DoubleHolder p2, DoubleHolder p3)
+	{
+		System.out.println("myBasicTypeImpl.f6()");
+		return 0.0;
+	}
 
 
-    public char f7 (char p1, org.omg.CORBA.CharHolder p2, org.omg.CORBA.CharHolder p3)
-    {
-	System.out.println("myBasicTypeImpl.f7()");
-	return 0;
-    }
+    public char f7(char p1, CharHolder p2, CharHolder p3)
+	{
+		System.out.println("myBasicTypeImpl.f7()");
+		return 0;
+	}
 
 
-    public String f8 (String p1, org.omg.CORBA.StringHolder p2, org.omg.CORBA.StringHolder p3)
-	throws CCMException
-    {
-	System.out.println("myBasicTypeImpl.8()");
-	p3.value = p2.value;
-	p2.value = p1;
-	String result = p3.value + p1;
-	return result;
-    }
+    public String f8(String p1, StringHolder p2, StringHolder p3) 
+    	throws CCMException
+	{
+		System.out.println("myBasicTypeImpl.8()");
+		p3.value = p2.value;
+		p2.value = p1;
+		String result = p3.value + p1;
+		return result;
+	}
 
-    public boolean f9 (boolean p1, org.omg.CORBA.BooleanHolder p2, org.omg.CORBA.BooleanHolder p3)
-    {
-	System.out.println("myBasicTypeImpl.9()");
-	return false;
-    }
+    public boolean f9(boolean p1, BooleanHolder p2, BooleanHolder p3)
+	{
+		System.out.println("myBasicTypeImpl.9()");
+		return false;
+	}
 
 
-    public byte f10 (byte p1, org.omg.CORBA.ByteHolder p2, org.omg.CORBA.ByteHolder p3)
-    {
-	System.out.println("myBasicTypeImpl.f10()");
-	return 0;
-    }
+    public byte f10(byte p1, ByteHolder p2, ByteHolder p3)
+	{
+		System.out.println("myBasicTypeImpl.f10()");
+		return 0;
+	}
 }
--- ccmtools/test/JavaClientLib/receptacle_module_types/src/client/myUserTypeImpl.java
+++ ccmtools/test/JavaClientLib/receptacle_module_types/src/client/myUserTypeImpl.java
@@ -1,90 +1,92 @@
 package client;
 
+import org.omg.CORBA.IntHolder;
+
+import world.europe.austria.Address;
+import world.europe.austria.AddressHolder;
+import world.europe.austria.Color;
+import world.europe.austria.ColorHolder;
+import world.europe.austria.LongListHolder;
+import world.europe.austria.Person;
+import world.europe.austria.PersonHolder;
+import world.europe.austria.PersonListHolder;
+import world.europe.austria.StringListHolder;
 import ccm.local.Components.CCMException;
 
-public class myUserTypeImpl
-	implements world.europe.austria.ccm.local.UserTypeInterface
+public class myUserTypeImpl implements world.europe.austria.ccm.local.UserTypeInterface
 {
 
-    public myUserTypeImpl()
-    {
-    }
-    
-    public world.europe.austria.Color f1 (world.europe.austria.Color p1, 
-				   world.europe.austria.ColorHolder p2, 
-				   world.europe.austria.ColorHolder p3)
-    {
-	System.out.println("myUserTypeImpl.f1()");
-    }
-
-    
-    public world.europe.austria.Person f2 (world.europe.austria.Person p1, 
-				    world.europe.austria.PersonHolder p2, 
-				    world.europe.austria.PersonHolder p3)
-	throws CCMException
-    {
-	System.out.println("myUserTypeImpl.f2()");
-	p3.value = new Person(p2.value.id, p2.value.name);
-	
-	p2.value.id = p1.id;
-	p2.value.name = p1.name;
-	
-	Person result = new Person(p1.id + p3.value.id, p1.name + p3.value.name);
-	return result;
-    }
-
-
-    public world.europe.austria.Address f3 (world.europe.austria.Address p1, 
-				     world.europe.austria.AddressHolder p2, 
-				     world.europe.austria.AddressHolder p3)
-    {
-	System.out.println("myUserTypeImpl.f3()");
-    }
-
-    
-    public int[] f4 (int[] p1, world.europe.austria.LongListHolder p2, 
-	      world.europe.austria.LongListHolder p3)
-    {
-	System.out.println("myUserTypeImpl.f4()");
-    }
-
-
-    public String[] f5 (String[] p1, world.europe.austria.StringListHolder p2, 
-		 world.europe.austria.StringListHolder p3)
-	throws CCMException
-    {
-	System.out.println("myUserTypeImpl.f5()");
-	try
+	public myUserTypeImpl()
 	{
-	    String[] p3a = new String[3];
-	    p3a[0] = p2.value[0];
-	    p3a[1] = p2.value[1];
-	    p3a[2] = p2.value[2];
-	    p3.value = p3a;
-	    
-	    p2.value = p1;
-	    
-	    String[] result = {"Test", "Test", "Test"};
-	    return result;
 	}
-	catch(Exception e) 
+
+	public Color f1(Color p1, ColorHolder p2, ColorHolder p3)
 	{
-	    e.printStackTrace();
-	    throw new CCMException();
+		System.out.println("myUserTypeImpl.f1()");
+		return null;
 	}
-    }
 
+	public Person f2(Person p1, PersonHolder p2, PersonHolder p3) 
+		throws CCMException
+	{
+		System.out.println("myUserTypeImpl.f2()");
+		p3.value = new Person(p2.value.id, p2.value.name);
 
-    public world.europe.austria.Person[] f6 (world.europe.austria.Person[] p1, 
-				      world.europe.austria.PersonListHolder p2, 
-				      world.europe.austria.PersonListHolder p3)
-    {
-	System.out.println("myUserTypeImpl.f6()");
-    }
+		p2.value.id = p1.id;
+		p2.value.name = p1.name;
 
+		Person result = new Person(p1.id + p3.value.id, p1.name + p3.value.name);
+		return result;
+	}
 
-    public int f7 (int t1, org.omg.CORBA.IntHolder t2, org.omg.CORBA.IntHolder t3)
-    {
-	System.out.println("myUserTypeImpl.f7()");
-    }
+	public Address f3(Address p1, AddressHolder p2, AddressHolder p3)
+	{
+		System.out.println("myUserTypeImpl.f3()");
+		return null;
+	}
+
+	public int[] f4(int[] p1, LongListHolder p2, LongListHolder p3)
+	{
+		System.out.println("myUserTypeImpl.f4()");
+		return null;
+	}
+
+	public String[] f5(String[] p1, StringListHolder p2, StringListHolder p3)
+			throws CCMException
+	{
+		System.out.println("myUserTypeImpl.f5()");
+		try
+		{
+			String[] p3a = new String[3];
+			p3a[0] = p2.value[0];
+			p3a[1] = p2.value[1];
+			p3a[2] = p2.value[2];
+			p3.value = p3a;
+
+			p2.value = p1;
+
+			String[] result =
+			{
+					"Test", "Test", "Test"
+			};
+			return result;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			throw new CCMException();
+		}
+	}
+
+	public Person[] f6(Person[] p1, PersonListHolder p2, PersonListHolder p3)
+	{
+		System.out.println("myUserTypeImpl.f6()");
+		return null;
+	}
+
+	public int f7(int t1, IntHolder t2, IntHolder t3)
+	{
+		System.out.println("myUserTypeImpl.f7()");
+		return 0;
+	}
 }
