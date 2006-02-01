678781968b3a4813bae31b8e2a5a961c98fa0135
teiniker <>
2006-02-01 12:12:58 +0000

    added new test case

---
+++ ccmtools/test/JavaClientLib/supports_module_simple/src/Client.java
@@ -0,0 +1,60 @@
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.StringHolder;
+
+import world.europe.austria.ccm.local.*;
+import ccm.local.ServiceLocator;
+
+
+public class Client
+{
+    public static void main(String[] args)
+    {
+        ORB orb = ORB.init(args, null);
+        try {
+	    // Set up the ServiceLocator singleton
+            ServiceLocator.instance().setCorbaOrb(orb);
+	    
+            // Register homes to the HomeFinder
+            ccm.local.HomeFinder.instance()
+		.register_home(TestHomeFactory.create(),"myTestHome");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+	
+	try
+	{
+	    System.out.println("Client");
+	    
+	    ccm.local.Components.HomeFinder homeFinder = 
+		ccm.local.HomeFinder.instance();
+	    TestHome home = 
+		(TestHome) homeFinder.find_home_by_name("myTestHome");
+
+	    Test component = home.create();	    
+	    component.configuration_complete();
+
+	    {
+		String s = "1234567890";
+		int size = component.op1(s);
+		assert(s.length() == size);
+	    }
+
+	    component.remove();
+	    System.out.println("OK!");
+	}
+	catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+	finally
+	{
+	    // Unregister homes from the HomeFinder
+	    ccm.local.HomeFinder.instance().unregister_home("myTestHome");
+
+	    // Tear down the ServiceLocator singleton
+	    ServiceLocator.instance().destroy();
+	}
+    }
+}
