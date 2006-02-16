dbde4ad760e3e14893e52e3c1e82b57ce0cd0396
teiniker <>
2006-02-16 15:56:56 +0000

    Refactored Java local component generator

--- ccmtools/src/ccmtools/generator/java/test/local/TypesTest.java
+++ ccmtools/src/ccmtools/generator/java/test/local/TypesTest.java
@@ -17,7 +17,7 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-package ccmtools.test.JavaLocalGenerator;
+package ccmtools.generator.java.test.local;
 
 import ccmtools.test.CcmtoolsTestCase;
 
