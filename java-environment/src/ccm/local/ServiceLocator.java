aa229c4a208d80870562fe2b9716b577f88ed17d
teiniker <>
2006-02-24 12:38:44 +0000

    Added createSessionComponentPoa method

--- java-environment/src/ccm/local/ServiceLocator.java
+++ java-environment/src/ccm/local/ServiceLocator.java
@@ -1,12 +1,18 @@
 package ccm.local;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.Policy;
 import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.ImplicitActivationPolicyValue;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
 
 import Components.CCMHome;
 import Components.CCMHomeHelper;
@@ -22,8 +28,12 @@ public class ServiceLocator
 	/** Store CORBA specific references */
 	private ORB orb = null;
 	
+	/** Map of created POAs */
+	private Map poaMap = new HashMap();
+		
 	/** Thread which calls orb.run() */
 	private Thread orbRunThread = null;
+	
 	
 	
 	/** Get an instance of this component proxy (singleton) */
@@ -44,6 +54,8 @@ public class ServiceLocator
 	{
 		orb = null;
 		instance = null;
+		logger = null;
+		poaMap.clear();
 	}
 	
 	/** Don't let clients use this constructor */
@@ -141,6 +153,46 @@ public class ServiceLocator
 		catch (Exception e)
 		{
 			//e.printStackTrace();
+			throw new ServiceLocatorException(e.getMessage());
+		}
+	}
+
+	
+	/**
+	 * Create a POA instance that supports all policies used for session
+	 * CORBA components.
+	 * Note that POA instance names has to be unique for a given ORB, thus
+	 * we use a map to store all created POAs.
+	 * 
+	 * @param name
+	 * @return
+	 * @throws ServiceLocatorException
+	 */
+	public POA createSessionComponentPoa(String name)
+		throws ServiceLocatorException
+	{
+		try
+		{
+			if (poaMap.containsKey(name))
+			{
+				return (POA) poaMap.get(name);
+			}
+			else
+			{
+				POA rootPoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+				rootPoa.the_POAManager().activate();
+				Policy[] policies = new Policy[1];
+				policies[0] = rootPoa
+						.create_implicit_activation_policy(ImplicitActivationPolicyValue.IMPLICIT_ACTIVATION);
+				POA sessionPoa = rootPoa.create_POA(name, rootPoa.the_POAManager(), policies);
+				sessionPoa.the_POAManager().activate();
+				poaMap.put(name, sessionPoa);
+				return sessionPoa;
+			}
+		}
+		catch (Exception e)
+		{
+			// e.printStackTrace();
 			throw new ServiceLocatorException(e.getMessage());
 		}
 	}
