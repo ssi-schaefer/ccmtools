fe8f28ae02caed3c9ba5be517972f3372646e710
teiniker <>
2006-03-21 13:32:34 +0000

    changed test cases to support sequence mapping to List

--- ccmtools/test/JavaClientLib/supports_module_types/src/Client.java
+++ ccmtools/test/JavaClientLib/supports_module_types/src/Client.java
@@ -3,11 +3,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.omg.CORBA.ORB;
+
 import world.europe.austria.ccm.local.*;
 import ccm.local.ServiceLocator;
 import ccm.local.Holder;
-
-import world.europe.austria.Person;
 
 public class Client
 {
@@ -192,54 +191,45 @@ public class Client
 	    }
 	    
 	    { // struct Person { long id; string name; }
-		world.europe.austria.Person p1 = 
-		    new world.europe.austria.Person(3, "Egon");
-		Holder<world.europe.austria.Person> p2 = 
-		    new Holder<world.europe.austria.Person>(
-			new world.europe.austria.Person(23, "Andrea"));
-		Holder<world.europe.austria.Person> p3 = 
-		    new Holder<world.europe.austria.Person>();
+		Person p1 = new Person(3, "Egon");
+		Holder<Person> p2 = new Holder<Person>(new Person(23, "Andrea"));
+		Holder<Person> p3 = new Holder<Person>();
 		
-		world.europe.austria.Person result = component.fu2(p1, p2, p3);
+		Person result = component.fu2(p1, p2, p3);
 		
-		assert(p3.getValue().name.equals("Andrea"));
-		assert(p3.getValue().id == 23);
-		assert(p2.getValue().name.equals("Egon"));
-		assert(p2.getValue().id == 3);
-		assert(result.name.equals("EgonAndrea"));
-		assert(result.id == 3 + 23);
+		assert(p3.getValue().getName().equals("Andrea"));
+		assert(p3.getValue().getId() == 23);
+		assert(p2.getValue().getName().equals("Egon"));
+		assert(p2.getValue().getId() == 3);
+		assert(result.getName().equals("EgonAndrea"));
+		assert(result.getId() == 3 + 23);
 	    }
 	    
 	    { // struct Address { string street; long number; Person resident; }
-		world.europe.austria.Person egon = 
-		    new world.europe.austria.Person(3, "Egon");
-		world.europe.austria.Address p1 = 
-		    new world.europe.austria.Address("Waltendorf", 7, egon);
+		Person egon = new Person(3, "Egon");
+		Address p1 = new Address("Waltendorf", 7, egon);
 		
-		world.europe.austria.Person andrea = 
-		    new world.europe.austria.Person(23, "Andrea");
-		Holder<world.europe.austria.Address> p2 = 
-		    new Holder<world.europe.austria.Address>(
-			 new world.europe.austria.Address("Petersgasse", 17, andrea));
+		Person andrea = new Person(23, "Andrea");
+		Holder<Address> p2 = new Holder<Address>(new Address("Petersgasse", 17, andrea));
 		
-		Holder<world.europe.austria.Address> p3 = new Holder<world.europe.austria.Address>();
+		Holder<Address> p3 = new Holder<Address>();
 		
-		world.europe.austria.Address result = component.fu3(p1, p2, p3);
+		Address result = component.fu3(p1, p2, p3);
 		
-		assert(p3.getValue().street.equals("Petersgasse"));
-		assert(p3.getValue().number == 17);
-		assert(p3.getValue().resident.name.equals("Andrea"));
-		assert(p3.getValue().resident.id == 23);
+		assert(p3.getValue().getStreet().equals("Petersgasse"));
+		assert(p3.getValue().getNumber() == 17);
+		assert(p3.getValue().getResident().getName().equals("Andrea"));
+		assert(p3.getValue().getResident().getId() == 23);
 		
-		assert(p2.getValue().street.equals("Waltendorf"));
-		assert(p2.getValue().number == 7);
-		assert(p2.getValue().resident.name.equals("Egon"));
-		assert(p2.getValue().resident.id == 3);
+		assert(p2.getValue().getStreet().equals("Waltendorf"));
+		assert(p2.getValue().getNumber() == 7);
+		assert(p2.getValue().getResident().getName().equals("Egon"));
+		assert(p2.getValue().getResident().getId() == 3);
 		
-		assert(result.street.equals("WaltendorfPetersgasse"));
-		assert(result.number == 24);
-		assert(result.resident.name.equals("EgonAndrea"));
-		assert(result.resident.id == 26);
+		assert(result.getStreet().equals("WaltendorfPetersgasse"));
+		assert(result.getNumber() == 24);
+		assert(result.getResident().getName().equals("EgonAndrea"));
+		assert(result.getResident().getId() == 26);
 	    }
 	    
 	    { // typedef sequence<long> LongList
@@ -314,18 +304,18 @@ public class Client
 		
 		for (int i = 0; i < result.size(); i++)
 		{
-		    assert(result.get(i).name.equals("Test"));
-		    assert(result.get(i).id == i);
+		    assert(result.get(i).getName().equals("Test"));
+		    assert(result.get(i).getId() == i);
 		}
 		for (int i = 0; i < p2.getValue().size(); i++)
 		{
-		    assert(p2.getValue().get(i).name.equals("Andrea"));
-		    assert(p2.getValue().get(i).id == i);
+		    assert(p2.getValue().get(i).getName().equals("Andrea"));
+		    assert(p2.getValue().get(i).getId() == i);
 		}
 		for (int i = 0; i < p3.getValue().size(); i++)
 		{
-		    assert(p3.getValue().get(i).name.equals("Egon"));
-		    assert(p3.getValue().get(i).id == i + i);
+		    assert(p3.getValue().get(i).getName().equals("Egon"));
+		    assert(p3.getValue().get(i).getId() == i + i);
 		}
 	    }
 
