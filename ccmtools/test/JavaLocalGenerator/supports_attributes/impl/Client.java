7f621218737254371fe27abe7b9f621eaffb1f41
teiniker <>
2006-03-23 13:57:30 +0000

    changed test case to support arrays

--- ccmtools/test/JavaLocalGenerator/supports_attributes/impl/Client.java
+++ ccmtools/test/JavaLocalGenerator/supports_attributes/impl/Client.java
@@ -113,6 +113,7 @@ public class Client
 			    Color result = component.color_value();
 			    assert(result == value);
 			}
+
 			{
 			    // struct Person { long id; string name; }
 			    Person value = new Person(3, "Egon");
@@ -121,6 +122,7 @@ public class Client
 			    assert(result.getId() == value.getId());
 			    assert(result.getName().equals(value.getName()));
 			}
+
 			{
 			    // struct Address { string street; long number; Person resident; }
 			    String street = "Waltendorf";
@@ -149,6 +151,7 @@ public class Client
 				assert(result.get(i) == value.get(i));
 			    }
 			}
+
 			{
 			    // typedef sequence<string> StringList
 			    List<String> value = new ArrayList<String>(10);
@@ -163,6 +166,7 @@ public class Client
 				assert(result.get(i).equals(value.get(i)));
 			    }
 			}
+
 			{
 			    // typedef sequence<Person> PersonList
 			    List<Person> value = new ArrayList<Person>(10);
@@ -178,6 +182,7 @@ public class Client
 				assert(result.get(i).getName().equals(value.get(i).getName()));
 			    }
 			}
+
 			{
 			    // typedef long time_t;
 			    int value = -7777;
@@ -185,8 +190,56 @@ public class Client
 			    int result = component.time_t_value();
 			    assert(result == value);
 			}
-			System.out.println("OK!");
 
+			{
+			    // typedef long LongArray[10]
+			    int length = 10;
+			    int[] value = new int[length];
+			    for(int i = 0; i< value.length; i++)
+			    {
+				value[i] = i;
+			    }
+			    component.longArray_value(value);
+			    int[] result = component.longArray_value();
+			    for(int i = 0; i<result.length; i++)
+			    {
+				assert(result[i] == value[i]);
+			    }
+			}
+
+			{
+			    // typedef string StringArray[10]
+			    int length = 10;
+			    String[] value = new String[length];
+			    for(int i = 0; i< value.length; i++)
+			    {
+				value[i] = "Egon";
+			    }
+			    component.stringArray_value(value);
+			    String[] result = component.stringArray_value();
+			    for(int i = 0; i<result.length; i++)
+			    {
+				assert(result[i].equals(value[i]));
+			    }
+			}
+
+			{
+			    // typedef Person PersonArray[10]
+			    Person[] value = new Person[10];
+			    for(int i = 0; i< value.length; i++)
+			    {
+				value[i] = new Person(i, "Andrea");
+			    }
+			    component.personArray_value(value);
+			    Person[] result = component.personArray_value();
+			    for(int i = 0; i < result.length; i++)
+			    {
+				assert(result[i].getId() == value[i].getId());
+				assert(result[i].getName().equals(value[i].getName()));
+			    }
+			}
+
+			System.out.println("OK!");
 			component.remove();
 		}
 		catch (java.lang.Exception e)
