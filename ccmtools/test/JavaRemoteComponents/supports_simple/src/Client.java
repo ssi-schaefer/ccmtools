28ed40a45568774a0aafe5e5cea538f5df8f5cdb
teiniker <>
2006-04-27 08:06:41 +0000

    added test case

---
+++ ccmtools/test/JavaRemoteComponents/supports_simple/src/Client.java
@@ -0,0 +1,91 @@
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.StringHolder;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+import world.ccm.local.*;
+import ccm.local.ServiceLocator;
+
+import java.util.logging.*;
+
+public class Client
+{
+    private static boolean isTest;
+
+    public static void main(String[] args)
+    {
+	System.out.println("Test Client");
+
+	// Configure Logger
+	Logger logger = Logger.getLogger("ccm.local");
+	logger.setLevel(Level.FINER);
+	Handler handler = new ConsoleHandler();
+	handler.setLevel(Level.ALL);
+	handler.setFormatter(new ccm.local.MinimalFormatter());
+	logger.addHandler(handler);
+	ccm.local.ServiceLocator.instance().setLogger(logger);
+	
+        try 
+	{
+	    /**
+	     * Server-side code (Part 1)
+	     */
+
+	    // Set up the ServiceLocator singleton
+	    ORB orb = ORB.init(args, null);
+	    ServiceLocator.instance().setCorbaOrb(orb);
+
+       	    world.ccm.remote.TestHomeDeployment.deploy("myTestHome");
+	    TestHomeClientLibDeployment.deploy("myTestHome");
+	    System.out.println("> Server is running...");
+	    // orb.run();
+	}
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+        }
+
+
+        try 
+	{
+	    /**
+	     * Client-side code (co-located with clientlib)
+	     **/
+	    ccm.local.Components.HomeFinder homeFinder = ccm.local.HomeFinder.instance();
+            TestHome home = (TestHome) homeFinder.find_home_by_name("myTestHome");
+            Test component = home.create();
+            component.configuration_complete();
+
+	    String s = "1234567890";
+	    int size = component.foo(s);
+	    assert(s.length() == size);
+	    component.remove();
+        }
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+        }
+
+	
+	try
+	{
+
+	    /**
+	     * Server-side code (Part 2)
+	     */
+	    TestHomeClientLibDeployment.undeploy("myTestHome");
+	    world.ccm.remote.TestHomeDeployment.undeploy("myTestHome");
+	    System.out.println("OK!");
+	}
+	catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+	finally
+	{
+
+	    // Tear down the ServiceLocator singleton
+	    ServiceLocator.instance().destroy();
+	}
+    }
+}
