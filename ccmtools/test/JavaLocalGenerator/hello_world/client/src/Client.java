b138cbd7cda8e276f44fc87303e717cdfcc363fe
teiniker <>
2006-05-05 08:35:01 +0000

    added tutorial example

---
+++ ccmtools/test/JavaLocalGenerator/hello_world/client/src/Client.java
@@ -0,0 +1,45 @@
+import world.ccm.local.*;
+import ccm.local.Components.*;
+
+import java.util.logging.*;
+
+public class Client
+{
+	public static void main(String[] args)
+	{
+	    // Deploy local Java component
+	    try
+	    {
+		HelloWorldHomeDeployment.deploy("HelloWorld");
+	    }
+	    catch (java.lang.Exception e)
+	    {
+		e.printStackTrace();
+	    }
+
+	    // Use local Java component
+	    try
+	    {
+		HelloWorldHome home = 
+		    (HelloWorldHome) ccm.local.HomeFinder.instance().find_home_by_name("HelloWorld");
+
+		HelloWorld component = home.create();
+		Hello facet = component.provide_hello();
+		
+		{
+		    String msg = facet.sayHello();
+		    System.out.println(">> " + msg);
+		}
+		component.remove();
+	    }
+	    catch (java.lang.Exception e)
+	    {
+		e.printStackTrace();
+	    }
+	    finally
+	    {
+		// Undeploy local Java component
+		HelloWorldHomeDeployment.undeploy("HelloWorld");
+	    }
+	}
+}
