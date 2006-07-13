106042af01005911a212707326a1d3cc83268fdd
teiniker <>
2006-07-13 09:56:28 +0000

    changed version numbers

--- cpp-environment/ccm/CCM_Local/Makefile.py
+++ cpp-environment/ccm/CCM_Local/Makefile.py
@@ -5,7 +5,7 @@
 
 try:
     PACKAGE_NAME('ccm-local-runtime')
-    PACKAGE_VERSION('0.6.6')
+    PACKAGE_VERSION('0.7.0')
 except Error, e:
     # we'll get here if this is not the package root, and have to
     # ignore this.
