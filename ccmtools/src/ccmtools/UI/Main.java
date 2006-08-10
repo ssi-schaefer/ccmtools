aa7f71aa4530a376ad076055dc664544535728a4
teiniker <>
2006-08-10 08:06:38 +0000

    Bugfix: cpp > stdout

--- ccmtools/src/ccmtools/UI/Main.java
+++ ccmtools/src/ccmtools/UI/Main.java
@@ -22,6 +22,7 @@ package ccmtools.UI;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -181,8 +182,11 @@ public class Main
                 
                 // step (0). run the C preprocessor on the input file.
                 // Run the GNU preprocessor cpp in a separate process.
-                String cmd = CcmtoolsProperties.Instance().get("ccmtools.cpp") + " -o "+ idlfile 
-                                + " " + includePath	+ " " + source;
+//                String cmd = CcmtoolsProperties.Instance().get("ccmtools.cpp") + " -o "+ idlfile 
+//                                + " " + includePath	+ " " + source;
+                String cmd = CcmtoolsProperties.Instance().get("ccmtools.cpp") 
+                                + " " + includePath + " " + source;
+
                 logger.fine(cmd);
                 uiDriver.printMessage(cmd);
                 Process preproc = Runtime.getRuntime().exec(cmd);
@@ -190,9 +194,13 @@ public class Main
                 BufferedReader stdError = new BufferedReader(new InputStreamReader(preproc.getErrorStream()));
 
                 // Read the output and any errors from the command
+                StringBuffer code = new StringBuffer();
                 String s;
                 while((s = stdInput.readLine()) != null)
-                    uiDriver.printMessage(s);
+                {
+//                    uiDriver.printMessage(s);
+                    code.append(s).append("\n");
+                }
                 while((s = stdError.readLine()) != null)
                     uiDriver.printMessage(s);
 
@@ -200,10 +208,17 @@ public class Main
                 // value of the attempted command
                 preproc.waitFor();
                 if(preproc.exitValue() != 0)
-                   throw new CcmtoolsException("Preprocessor: "
-                                + "Please verify your include paths or file names ("
+                {
+                   throw new CcmtoolsException("Preprocessor: Please verify your include paths or file names ("
                                 + source + ").");
-
+                }
+                else
+                {
+//                    System.out.println(">>>> Write:\n" + code);
+                    FileWriter writer = new FileWriter(idlfile);
+                    writer.write(code.toString(), 0, code.toString().length());
+                    writer.close();
+                }
                 
                 // step (1). parse the resulting preprocessed file.
                 uiDriver.printMessage("parse " + idlfile.toString());
