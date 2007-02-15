c73fbae0ad07f56e846e0930acb7339ebc887da1
rlechner <>
2007-02-15 07:24:07 +0000

    checks the pretty printer

--- ccmtools/test/AssemblyParser/build-parser.sh
+++ ccmtools/test/AssemblyParser/build-parser.sh
@@ -4,6 +4,9 @@ thisdir=$(pwd)
 root="$thisdir/../.."
 libdir="$root/lib"
 
+cd $root
+ant jar.AssemblyMain || exit 1
 export CLASSPATH=$libdir/java-cup-11a.jar:$libdir/assembly.jar
-java ccmtools.parser.assembly.Main "$@" || exit 1
+
+cd $thisdir
 
