ed0f7e2c8b2ae6185a96603022c7dd2795d67655
rlechner <>
2007-03-20 14:06:13 +0000

    component adapter now uses ComponentDelegator

--- ccmtools/src/ccmtools/generator/java/templates/ProvidesDefEquivalentMethodAdapterLocalTemplate.java
+++ ccmtools/src/ccmtools/generator/java/templates/ProvidesDefEquivalentMethodAdapterLocalTemplate.java
@@ -17,13 +17,17 @@ public class ProvidesDefEquivalentMethodAdapterLocalTemplate
   protected final String TEXT_1 = NL + "    public ";
   protected final String TEXT_2 = " provide_";
   protected final String TEXT_3 = "()" + NL + "    {" + NL + "        logger.fine(\"\");" + NL + "        if(";
-  protected final String TEXT_4 = "FacetAdapter == null)" + NL + "        {";
-  protected final String TEXT_5 = NL + "            ";
-  protected final String TEXT_6 = "FacetAdapter = " + NL + "                new ";
-  protected final String TEXT_7 = "Adapter(localInterface.get_";
-  protected final String TEXT_8 = "());" + NL + "        }" + NL + "        return ";
-  protected final String TEXT_9 = "FacetAdapter;" + NL + "    }";
-  protected final String TEXT_10 = NL;
+  protected final String TEXT_4 = "FacetAdapter == null)" + NL + "        {" + NL + "        \tif(delegator!=null)" + NL + "        \t{" + NL + "        \t\tObject o = delegator.provide(\"";
+  protected final String TEXT_5 = "\");" + NL + "        \t\tif(o!=null)" + NL + "        \t\t{" + NL + "        \t\t\t";
+  protected final String TEXT_6 = "FacetAdapter =" + NL + "        \t\t\t\t(";
+  protected final String TEXT_7 = ")o;" + NL + "        \t\t\treturn ";
+  protected final String TEXT_8 = "FacetAdapter;" + NL + "        \t\t}" + NL + "        \t}";
+  protected final String TEXT_9 = NL + "            ";
+  protected final String TEXT_10 = "FacetAdapter = " + NL + "                new ";
+  protected final String TEXT_11 = "Adapter(localInterface.get_";
+  protected final String TEXT_12 = "());" + NL + "        }" + NL + "        return ";
+  protected final String TEXT_13 = "FacetAdapter;" + NL + "    }";
+  protected final String TEXT_14 = NL;
 
   public String generate(Object argument)
   {
@@ -36,6 +40,7 @@ public class ProvidesDefEquivalentMethodAdapterLocalTemplate
     stringBuffer.append(TEXT_3);
     stringBuffer.append(provides.getIdentifier());
     stringBuffer.append(TEXT_4);
+    stringBuffer.append(provides.getIdentifier());
     stringBuffer.append(TEXT_5);
     stringBuffer.append(provides.getIdentifier());
     stringBuffer.append(TEXT_6);
@@ -43,9 +48,16 @@ public class ProvidesDefEquivalentMethodAdapterLocalTemplate
     stringBuffer.append(TEXT_7);
     stringBuffer.append(provides.getIdentifier());
     stringBuffer.append(TEXT_8);
-    stringBuffer.append(provides.getIdentifier());
     stringBuffer.append(TEXT_9);
+    stringBuffer.append(provides.getIdentifier());
     stringBuffer.append(TEXT_10);
+    stringBuffer.append(provides.getInterface().generateAbsoluteJavaName());
+    stringBuffer.append(TEXT_11);
+    stringBuffer.append(provides.getIdentifier());
+    stringBuffer.append(TEXT_12);
+    stringBuffer.append(provides.getIdentifier());
+    stringBuffer.append(TEXT_13);
+    stringBuffer.append(TEXT_14);
     return stringBuffer.toString();
   }
 }
---
+++ java-environment/src/Components/ComponentDelegator.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Mar 20, 2007
+ * 
+ * R&D Salomon Automation (http://www.salomon.at)
+ * 
+ * Robert Lechner (robert.lechner@salomon.at)
+ * 
+ * $Id$
+ */
+package Components;
+
+/**
+ * If the implementation class of the component implements this interface, the local component
+ * adapter delegates to it.
+ */
+public interface ComponentDelegator
+{
+    /**
+     * provides a facet
+     * 
+     * @param name name of the facet
+     * @return facet adapter (or null if the facet is unknown)
+     */
+    Object provide( String name );
+
+    /**
+     * connect a facet into a receptacle
+     * 
+     * @param name name of the receptacle
+     * @param connection the facet
+     * @return cookie for multiple receptacles; null for single receptacles
+     */
+    Cookie connect( String name, Object connection ) throws InvalidName, InvalidConnection,
+            AlreadyConnected, ExceededConnectionLimit;
+
+    /**
+     * disconnects from a receptacle
+     * 
+     * @param name name of the receptacle
+     * @param ck cookie for multiple receptacles; null for single receptacles
+     */
+    void disconnect( String name, Cookie ck ) throws InvalidName, InvalidConnection,
+            CookieRequired, NoConnection;
+}
