e088cace57060f03d8bd9a92868c17bf071ea6de
cvs2git <>
2007-03-28 16:42:27 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE-0_8_8'.

--- java-environment/src/Components/AssemblyFactory.java
+++
@@ -1,7 +0,0 @@
-package Components;
-
-public interface AssemblyFactory
-{
-	 Assembly create()
-		throws CreateFailure;
-}
--- java-environment/src/Components/HomeExecutorBase.java
+++
@@ -1,9 +0,0 @@
-package Components;
-
-/***
- * The HomeExecutorBase is a common base for all home implementations.
- * CCM Specification 3-40
- */
-public interface HomeExecutorBase
-{
-}
